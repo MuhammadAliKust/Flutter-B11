ad4e59629f9de471f35bd7c7ea2cf0d7b6b5e6d6
Muhammad Ali Nizami <kustalics@gmail.com>
2024-12-21 15:30:48 +0500

    Passing Pram via Constructor

--- lib/screen_a.dart
+++ lib/screen_a.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_b11/screen_b.dart';
 
 class ScreenAView extends StatelessWidget {
-  const ScreenAView({super.key});
+  ScreenAView({super.key});
+
+  String name = "Khurram";
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +15,13 @@ class ScreenAView extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ScreenBView()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ScreenBView(
+                            name: name,
+                        email: 'test@mail.com',
+                          )));
             },
             child: Text("Go to Screen B")),
       ),
--- lib/screen_b.dart
+++ lib/screen_b.dart
@@ -3,40 +3,56 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class ScreenBView extends StatelessWidget {
-  const ScreenBView({super.key});
+  final String name;
+  final String email;
+
+  const ScreenBView({super.key, required this.name, required this.email});
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () {
-        showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("Message"),
-                content: Text("Do you really want to exit from app?"),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        exit(0);
-                      },
-                      child: Text("Yes")),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("No")),
-                ],
-              );
-            });
+        // showDialog(
+        //     context: context,
+        //     builder: (context) {
+        //       return AlertDialog(
+        //         title: Text("Message"),
+        //         content: Text("Do you really want to exit from app?"),
+        //         actions: [
+        //           TextButton(
+        //               onPressed: () {
+        //                 exit(0);
+        //               },
+        //               child: Text("Yes")),
+        //           TextButton(
+        //               onPressed: () {
+        //                 Navigator.pop(context);
+        //               },
+        //               child: Text("No")),
+        //         ],
+        //       );
+        //     });
 
-        return Future.value(false);
+        return Future.value(true);
       },
       child: Scaffold(
         appBar: AppBar(
           leading: SizedBox(),
           leadingWidth: 0,
           title: Text("Screen B"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                name,
+                style: TextStyle(fontSize: 30),
+              ), Text(
+                email,
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
         ),
       ),
     );
